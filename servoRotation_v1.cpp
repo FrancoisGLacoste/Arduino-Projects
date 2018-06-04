278755f2d5ed24e7dbd1683a967eb1ef72d79e17
FrancoisGLacoste <38571323+FrancoisGLacoste@users.noreply.github.com>
2018-06-03 23:24:30 -0400

    Add files via upload
    A class that manage the rotation of a servo, without potentiometer

---
+++ servoRotation_v1.cpp
@@ -0,0 +1,129 @@
+//  servoRotation_v1.cpp
+
+#include <Servo.h>
+#include <StandardCplusplus.h> // standard libraries for Arduino
+#include <vector>
+
+// output: digital pin for servo
+void servoRotation::setServo(uint8_t pin){
+
+    servoPin =pin ; // servoPin is a member of servoRotation class
+    attach(servoPin);  // attach() :  is a fct member of Servo class: attach the servo on servoPin.
+
+    angleVect[0]=read();   // read() is a function member of Servo class
+    deltatVect[0] = 0; // member of servoRotation class
+}
+
+
+// input: analog pin for potentiometer (the wire in middle in the potentiometer)
+void servoRotation::setPot(uint8_t pin1, uint8_t pin2){
+   potPin = pin1;  
+   potControlPin=pin2;
+   
+}
+
+/*   
+* omega : angular velocity [omega degree per sgn sec] between angle and angle+sgn
+* generally we take sgn =  +1 or -1 so that omega is in degree/sec
+*/ 
+uint8_t servoRotation::turnServo1deg(int sgn,uint8_t omega=60 ){
+  uint8_t tt = 1000/omega  ;    // [ms]
+  angle  +=sgn;   
+  servo.write(angle); 
+  delay(tt);   
+  return angle;
+
+} 
+ 
+/*  Delta : angular variation  [degrees]
+ *  when Delta > 0: turn right; 
+ *  when Delta <0:  turn left;                               
+ *  omega:  constant angular velocity [degree/s]
+ */
+void servoRotation::turnServo(int Delta =1, uint8_t omega =60){
+  uint8_t angle, tt;
+  int sgn ;
+  tt = 1000/omega  ; //[ms]  
+  sgn = sign(Delta);  // =1 ,  -1, or 0
+  angle = servo.read();
+  while ( (angle != (angle+Delta)) && (angle>0)&& (angle<180) ){
+    servo.write(angle +sgn); 
+    delay(tt);   
+  }
+}
+
+/* Actualize the servo from current position to angular position theta1.  
+ *  omega :  constant angular velocity   [degree/s ]
+ *  
+ * (very similar to turnServo() )
+ *                                                           */ 
+void servoRotation::actualizeServo(uint8_t theta1=90, uint8_t omega=60) {
+  uint8_t theta0 , tt , angle; 
+  int Delta, sgn;
+   
+  theta0 = servo.read();     // current angular position
+  Delta =  theta1 - theta0  ; //  90 - 93 = 253 if delta is uint8_t
+  sgn = sign(Delta) ;
+  tt = 1000/omega  ;  // [ms per degree]; inverse of angular velocity
+  angle =theta0; 
+  while (  abs(theta1-angle)>0){ 
+      angle+=sgn;
+      servo.write(angle); 
+      delay(tt);   
+  }
+}   
+
+
+/* n scanning left-right   */
+void servoRotation::scanLeftRight(uint8_t n=1,uint8_t theta0 = 0,uint8_t theta1= 180, uint8_t omega=60, bool sonar=false){
+    uint8_t angle;
+    int sgn =1;
+
+    // update member value:
+    omega0=omega1;
+    omega1 =omega; 
+    angle = servo.read();  
+    
+   
+      // n scans :
+
+      // first reaches the range of angles to scan. 
+      if ( angle <= theta0){
+          actualizeServo(theta0+1, omega );
+          angle = theta0+1; //max(theta0+1, 0);
+      }
+      else if ( angle >= theta1) {
+          actualizeServo(theta1 - 1, omega ); 
+          angle = theta1-1; // min(theta1-1, 179);
+          sgn=-1;
+      }//if
+  /*    Serial.print(   "scanLeftRight:   ");   //DEBUG
+      Serial.println(angle);   //DEBUG
+   */   
+      for (uint8_t i=0; i<n; i++){
+         while ( min(abs(angle - theta0),abs(angle - theta1))>0  ){
+              angle = turnServo1deg(angle, sgn , omega);
+              //angle +=sgn ;
+   //           Serial.println(angle);   //DEBUG
+         
+         } 
+         // at this point:  (angle ==theta0)||(angle==theta1) 
+         sgn *=(-1) ;// hence sgn alternates 1, -1, 1, -1....     
+                      
+      }//for
+         
+}
+
+
+
+/*  with variable angular velocity  
+ *  thetaArray:  angular position at each time step 
+ *  omegaArray:  angular velocity at each time step 
+ *  In order to have omegaArray == thetaArray, we assign a zero velocity to the last index..  
+ */
+
+/*
+uint8_t servoRotation::actualizeServoNonCst(uint8_t thetaArray[]  , uint8_t ttArray[] ) ;
+
+*/
+
---
+++ servoRotation_v1.h
@@ -0,0 +1,51 @@
+//  servoRotation_v1.h
+ 
+
+#include <Servo.h> 
+
+//    Include Guard
+#ifndef __servoRot_H_INCLUDED__  
+#define __servoRot_H_INCLUDED__  
+
+#include <StandardCplusplus.h> // standard libraries for Arduino
+#include <vector>
+
+// data type for servo position acquisition vs time
+typedef vector<uint8_t> uint8vect;
+  
+class servoRotation: protected Servo {
+  // public members of Servo object are only accessible to servoRotation class, not to loop()
+  // in Servo :  read() ; write();  attach() 
+  
+  private: 
+    // pins definition
+    const uint8_t servoPin ;        // digital output: pin for servo position
+
+    // private methods: 
+    uint8_t turnServo1deg(int );  
+
+  protected:
+    uint8_t  angle ; 
+    uint8_t  omega0; //  angular velocity from step -1 to step 0
+    uint8_t  omega1; //  angular velocity from step 0 to step 1 
+  
+    //eventually for data acquisition of servo position ....  
+    // when serial communication is possible, (angle, deltat) should be directly send instead of accumulating the data in these vectors:
+    uint8vect angleVect  ;
+    uint8vect deltatVect ;// deltatVect[0]=0
+    // time difference betweem each angle measures. storing time itself would take more memory (more than 8 bit per step )
+
+  public: 
+    // The constructor is not explicitly defined. 
+    
+    void setServo(uint8_t )
+    
+    void turnServo(int , uint8_t )
+    void actualizeServo(uint8_t , uint8_t ) 
+    void scanLeftRight(uint8_t ,uint8_t ,uint8_t , bool )         
+    void actualizeServoNonCst(uint8_t []  , uint8_t [] ) 
+  
+};
+
+
+#endif 
